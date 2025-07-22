7bf1d8befba907b82abbe5637ab74d024c58fbb5
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-07-22 19:04:38 -0400

    Update download-steps.js

--- app/js/download-steps.js
+++ app/js/download-steps.js
@@ -1,12 +1,12 @@
-function closeWhopPwaPopup() {
-  const popup = document.getElementById('whop-pwa-popup');
+function closeMoMPwaPopup() {
+  const popup = document.getElementById('MoM-pwa-popup');
   popup.classList.remove('active');
-  localStorage.setItem('hideWhopPwaPopup', '1');
+  localStorage.setItem('hideMoMPwaPopup', '1');
   setTimeout(() => popup.style.display = 'none', 350);
 }
 
 (function() {
-  if (localStorage.getItem('hideWhopPwaPopup') === '1') return;
+  if (localStorage.getItem('hideMoMPwaPopup') === '1') return;
 
   const isIos = /iphone|ipad|ipod/i.test(navigator.userAgent) && !window.MSStream;
   const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
@@ -18,10 +18,10 @@ function closeWhopPwaPopup() {
 
   // Only show if user is on iOS Safari or Android Chrome and not already installed
   if ((isIos && isSafari && !isInStandaloneMode()) || (isAndroid && !isInStandaloneMode())) {
-    const popup = document.getElementById('whop-pwa-popup');
+    const popup = document.getElementById('MoM-pwa-popup');
     popup.style.display = 'block';
     setTimeout(() => popup.classList.add('active'), 100);
 
-    document.getElementById('whop-pwa-close').onclick = closeWhopPwaPopup;
+    document.getElementById('MoM-pwa-close').onclick = closeMoMPwaPopup;
   }
 })();
