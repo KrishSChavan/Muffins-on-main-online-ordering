d59f95d62ec3e59e49de28989676278cf23f6e80
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-07-27 23:00:18 -0400

    Update buttons.js

--- admin-app/js/edit-menu/buttons.js
+++ admin-app/js/edit-menu/buttons.js
@@ -120,28 +120,3 @@ editItemForm.addEventListener('submit', (e) => {
   editItemModal.style.display = 'none';
   editItemForm.reset();
 });
-
-
-
-
-menuContainer.addEventListener('click', function(e) {
-  if (e.target.classList.contains('edit-btn')) {
-    const parentDiv = e.target.closest('.menu-item');
-    if (parentDiv) {
-      const id = parentDiv.id;
-      // console.log('Div ID:', id);
-      editItemModal.style.display = 'flex';
-      editItemForm.reset(); // Reset form fields
-      const item = allMenuData.find(item => item.id === id);
-      if (item) {
-        // Populate the edit form with item data
-        editItemForm.querySelector('input[name="name"]').value = item.name;
-        editItemForm.querySelector('textarea[name="description"]').value = item.description;
-        editItemForm.querySelector('input[name="price"]').value = item.price;
-        editItemForm.querySelector('input[name="category"]').value = item.category;
-        editItemForm.querySelector('input[name="hidden"]').checked = item.hidden;
-        editItemForm.dataset.itemId = id; // Store the ID for later use
-      }
-    }
-  }
-});
