4420c8d80a3e9395e89f9518830922f626e84f58
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-07-28 14:17:06 -0400

    Update update.js

--- admin-app/js/edit-menu/update.js
+++ admin-app/js/edit-menu/update.js
@@ -26,7 +26,6 @@ menuContainer.addEventListener('click', function(e) {
 menuContainer.addEventListener('click', function(e) {
   if (e.target.classList.contains('hide-checkbox')) {
     const parentDiv = e.target.closest('.menu-item');
-    console.log('Checkbox clicked for item:', parentDiv.id);
     if (parentDiv) {
       const id = parentDiv.id;
       socket.emit('toggle-item-visibility', id, e.target.checked);
@@ -43,4 +42,49 @@ menuContainer.addEventListener('click', function(e) {
       }
     }
   }
+});
+
+
+
+
+const deleteModal = document.getElementById('deleteModal');
+const deleteConfirmBtn = document.getElementById('confirm-delete-btn');
+const deleteCancelBtn = document.getElementById('cancel-delete-btn');
+const itemToDeleteText = document.getElementById('item-to-delete-text');
+
+menuContainer.addEventListener('click', function(e) {
+  if (e.target.classList.contains('delete-btn')) {
+    const parentDiv = e.target.closest('.menu-item');
+
+    if (parentDiv) {
+      const id = parentDiv.id;
+      const item = allMenuData.find(item => item.id === id);
+      if (item) {
+        itemToDeleteText.textContent = item.name; // Display the item name in the modal
+        deleteModal.style.display = 'flex';
+        deleteConfirmBtn.dataset.itemId = id; // Store the ID for later use
+      }
+    }
+  }
+});
+
+
+deleteCancelBtn.addEventListener('click', () => {
+  deleteModal.style.display = 'none';
+});
+
+
+deleteConfirmBtn.addEventListener('click', () => {
+  const itemId = deleteConfirmBtn.dataset.itemId;
+  if (itemId) {
+    socket.emit('delete-item', itemId);
+    // Remove the item from allMenuData
+    allMenuData = allMenuData.filter(item => item.id !== itemId);
+    // Update the menuDataByCategories variable
+    Object.keys(menuDataByCategories).forEach(category => {
+      menuDataByCategories[category] = menuDataByCategories[category].filter(item => item.id !== itemId);
+    });
+    filterAndRender(); // Re-render the menu
+  }
+  deleteModal.style.display = 'none';
 });
