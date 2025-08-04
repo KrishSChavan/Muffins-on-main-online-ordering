31238c59ad526dc68b3509ab132ccd2a7df08d95
Krish <44930634+KrishSChavan@users.noreply.github.com>
2025-08-03 22:36:10 -0400

    Delete admin-app directory

--- admin-app/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  printReceipt: (html) => ipcRenderer.send('print-receipt', { html })
-  // printReceipt: (orderData) => ipcRenderer.invoke('print-receipt', orderData)
-});
